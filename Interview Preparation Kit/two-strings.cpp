0e7ca8ce20a81557586e1a988a1affe7b9e6ff35
Hrishikesh Suresh <hrishikesh123s@gmail.com>
2019-05-20 22:21:05 +0530

    adding few problems

---
+++ Interview Preparation Kit/two-strings.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the twoStrings function below.
+string twoStrings(string s1, string s2) {
+    unordered_map<char, int> word;
+    for(auto i: s1){
+        word[i]++;
+    }
+    for(auto i: s2){
+        if(word[i] > 0){
+            return "YES";
+        }
+    }
+    return "NO";
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int q;
+    cin >> q;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        string s1;
+        getline(cin, s1);
+
+        string s2;
+        getline(cin, s2);
+
+        string result = twoStrings(s1, s2);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
